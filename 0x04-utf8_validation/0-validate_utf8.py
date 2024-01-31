4bb79268d58ac2a1b007e66c9130b1b11d4f4c21
rugema3 <rugema61@gmail.com>
2024-01-31 14:57:30 +0200

    going back to my previous implementation.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,12 @@
 
 def validUTF8(data):
     """Check if dataset is a valid utf-8."""
-    num_bytes = 0
-
-    for num in data:
-        if num_bytes == 0:
-            if (num >> 5) == 0b110:
-                num_bytes = 1
-            elif (num >> 4) == 0b1110:
-                num_bytes = 2
-            elif (num >> 3) == 0b11110:
-                num_bytes = 3
-            elif (num >> 7) != 0:
+    try:
+        for num in data:
+            if num < 0 or num > 255:
                 return False
-        else:
-            if (num >> 6) != 0b10:
-                return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+        bytes_data = bytes(data)
+        bytes_data.decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
